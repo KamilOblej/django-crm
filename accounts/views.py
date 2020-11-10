ccd714bc44db15f60038c2670890e678ac8e84df
KamilOblej <grubyosno@gmail.com>
2020-11-11 00:28:47 +0100

    render data from database

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . models import Product, Order, Customer
+
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    oreders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+
+    total_orders = oreders.count()
+    delivered = oreders.filter(status='Delivered').count()
+    pending = oreders.filter(status='Pending').count()
+
+    context = {
+        'orders': oreders,
+        'customers': customers,
+        'total_customers': total_customers,
+        'total_orders': total_orders,
+        'delivered': delivered,
+        'pending': pending,
+    }
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def customer(request):
@@ -11,4 +30,6 @@ def customer(request):
 
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+
+    return render(request, 'accounts/products.html', {'products': products})
