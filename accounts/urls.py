04141d7ed8d6eaf6417eedd052f395df55e547b9
KamilOblej <grubyosno@gmail.com>
2020-11-11 01:16:35 +0100

    add order update

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', views.home, name='dashboard'),
     path('customer/<str:pk_test>/', views.customer, name='customer'),
     path('products/', views.products, name='products'),
-    path('create_order', views.create_order, name='create_order'),
+
+    path('create_order/', views.create_order, name='create_order'),
+    path('update_order/<str:pk>', views.update_order, name='update_order'),
 ]
